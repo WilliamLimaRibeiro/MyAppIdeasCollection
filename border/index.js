138533c563bcc51abeecc3ff710927f54493e038
William Ribeiro <rybeyro93@gmail.com>
2020-11-20 21:58:56 -0300

    Add files via upload

---
+++ border/index.js
@@ -0,0 +1,32 @@
+var topright;
+var bottomright;
+var topleft;
+var bottomleft;
+
+function topRight(){
+    topright = document.getElementById('top-right').value;
+    document.getElementById('box').style.borderTopRightRadius = topright + '%';
+    codFont()
+}
+
+function bottomRight() {
+    bottomright = document.getElementById('bottom-right').value;
+    document.getElementById('box').style.borderBottomRightRadius = bottomright + '%';
+    codFont()
+}
+
+function topLeft() {
+    topleft = document.getElementById('top-left').value;
+    document.getElementById('box').style.borderTopLeftRadius = topleft + '%';
+    codFont()
+}
+
+function bottomLeft() {
+    bottomleft = document.getElementById('bottom-left').value;
+    document.getElementById('box').style.borderBottomLeftRadius = bottomleft + '%';
+    codFont()
+}
+
+function codFont() {
+    document.getElementById('cod').value ="border-top-right-radius:"+ topright + "%; border-bottom-right-radius:" + bottomright +"%; border-bottom-left-radius:"+ bottomleft +"%; border-top-left-radius:"+topleft+"%;";
+}
